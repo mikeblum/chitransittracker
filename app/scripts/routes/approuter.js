0711f69382b074e02587d45866b0177f4d340f41
mblum <mblum@roguequery.com>
2014-01-01 17:25:23 -0600

    moved trainlines to trainstatus

--- app/scripts/routes/approuter.js
+++ app/scripts/routes/approuter.js
@@ -10,22 +10,6 @@ define([
 
 	var AppRouter = Backbone.Router.extend({
 		initialize: function() {
-			$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-			    options.crossDomain ={
-			        crossDomain: true
-			    };
-			    options.xhrFields = {
-			        withCredentials: true
-			    };
-			});
-
-			var allowCrossDomain = function(req, res, next) {
-			    res.header('Access-Control-Allow-Origin', config.allowedDomains);
-			    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-			    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-			    next();
-			}
 		},
 		routes: {
 			'' : 'trainStatus'
