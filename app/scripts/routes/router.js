d4c3de05525b916db54a09c318243025d261d990
mblum <mblum@roguequery.com>
2013-12-31 20:46:59 -0600

    css basics

---
+++ app/scripts/routes/router.js
@@ -0,0 +1,16 @@
+/*global define*/
+
+define([
+    'jquery',
+    'backbone'
+], function ($, Backbone) {
+    'use strict';
+
+    var RouterRouter = Backbone.Router.extend({
+        routes: {
+        }
+
+    });
+
+    return RouterRouter;
+});
